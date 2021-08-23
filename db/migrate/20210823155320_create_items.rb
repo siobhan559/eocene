a0dd64439162b15dfeba7b79c978f3b0624a49ba
Cesar Ades <kito@ades.as>
2021-08-23 16:53:58 +0100

    fix migrations for items table

--- db/migrate/20210823155320_create_items.rb
+++ db/migrate/20210823155320_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :description
       t.float :price
-      t.user :references
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
