f8077b453778f18f8e6838146fd2beed4478c38e
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-08-23 16:05:25 +0100

    Updated User schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 2021_08_23_143726) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -25,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_143726) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
