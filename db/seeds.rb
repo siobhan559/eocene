b0ac4ec6b9004ef8dab3f2ccb966db26dca03464
Cesar Ades <kito@ades.as>
2021-08-24 18:14:43 +0100

    Better seed and fixed image for product card

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,45 @@
-puts "Cleaning all items"
-Item.delete_all
-puts "Items cleaned"
+require "open-uri"
 
-user = User.create(
-  first_name: 'Cesar',
-  last_name: 'Ades',
+puts "Cleaning db"
+Item.delete_all
+User.delete_all
+puts "Db cleaned"
+
+user1 = User.create(
+  first_name: 'Seed',
+  last_name: 'One',
   password: 'password',
-  email: 'email@gmail.com'
+  email: 'one@seed.com'
 )
 
-50.times do
+file = URI.open('https://source.unsplash.com/featured/?face')
+user1.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
+
+user2 = User.create(
+  first_name: 'Seed',
+  last_name: 'Two',
+  password: 'password',
+  email: 'two@seed.com'
+)
+
+file = URI.open('https://source.unsplash.com/featured/?face')
+user2.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
+
+hash = [['Water', 'Surfboard'], ['Snow', 'Ski Boots'], ['Water', 'Wet Suit'], ['Snow', 'Snowboard'],
+        ['Free Fall', 'Parachute'], ['Mountain', 'Harness'], ['Mountain', 'Helmet'], ['Mountain', 'Bike Frame'], ['Mountain', 'Bike Pedals'], ['Water', 'Kayak']]
+
+25.times do
   item = Item.new(
-    name: %w[skis boots surfboard snowboard parachute rope helmet bike-frame bike-pedals].sample.capitalize,
-    description: Faker::Quote.famous_last_words,
+    name: hash.sample[1],
+    description: 'This is a really good description for this item.',
     price: (5..50).to_a.sample,
-    category: %w[surf trek mountain-climb ski snowboard mountain-bike skydive].sample.capitalize,
-    user: user
+    category: hash.sample[2],
+    user: [user1, user2].sample
   )
+  file = URI.open('https://source.unsplash.com/featured/?sport')
+  item.photos.attach(io: file, filename: 'image.png', content_type: 'image/png')
   item.save!
+  puts "Successfully added item: #{item.name}"
 end
 
 puts "Added #{Item.count} items"
