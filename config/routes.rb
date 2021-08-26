5722271f9d13662826175f69427f0fdd87a271c6
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-08-26 18:00:30 +0100

    updated the booking confirmation link when a booking is submitted

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :items do
-    resources :bookings, only: %i[show create update destroy]
+    resources :bookings, only: %i[create update destroy]
   end
+  resources :bookings, only: :show
   get 'account/:id/listings', to: 'pages#listings', as: :listings
   get 'account/:id/purchases', to: 'pages#purchases', as: :purchases
   get 'account/:id/settings', to: 'pages#settings', as: :settings
