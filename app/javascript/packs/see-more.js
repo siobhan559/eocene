e1fa8bb90229f92796d1c93ef54a588df48a9c60
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-08-26 15:07:11 +0100

    more styling on show page

--- app/javascript/packs/see-more.js
+++ app/javascript/packs/see-more.js
@@ -1,10 +1,24 @@
 const listenForSeeMore = () => {
   const seeMore = document.querySelector('.show-content');
   seeMore.addEventListener("click", (e) => {
+    const adjustBoxHeight = document.getElementById('info1');
     e.preventDefault();
+    console.log(adjustBoxHeight);
     // console.log(document.querySelector('.hide-content'));
     document.getElementById('hide').classList.toggle('hide-content');
+    // if (adjustBoxHeight.classList === "info-box") {
+    //   adjustBoxHeight.classList = "info-box2";
+    // } else {
+    //   adjustBoxHeight.classList = "info-box";
+    // };
   });
 };
 
 export { listenForSeeMore };
+// if (button.innerText === "Let's Book!") {
+//   button.innerText = "Hide booking form";
+//   button.classList = "call-to-action";
+// } else {
+//   button.innerText = "Let's Book!";
+//   button.classList = "call-to-action";
+// };
