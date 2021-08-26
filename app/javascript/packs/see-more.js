c500c5a1bcadde70687bfe64a3df85209010bc70
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-08-26 16:57:06 +0100

    Updated JS buttons and styling

--- app/javascript/packs/see-more.js
+++ app/javascript/packs/see-more.js
@@ -4,21 +4,9 @@ const listenForSeeMore = () => {
     const adjustBoxHeight = document.getElementById('info1');
     e.preventDefault();
     console.log(adjustBoxHeight);
-    // console.log(document.querySelector('.hide-content'));
     document.getElementById('hide').classList.toggle('hide-content');
-    // if (adjustBoxHeight.classList === "info-box") {
-    //   adjustBoxHeight.classList = "info-box2";
-    // } else {
-    //   adjustBoxHeight.classList = "info-box";
-    // };
+
   });
 };
 
 export { listenForSeeMore };
-// if (button.innerText === "Let's Book!") {
-//   button.innerText = "Hide booking form";
-//   button.classList = "call-to-action";
-// } else {
-//   button.innerText = "Let's Book!";
-//   button.classList = "call-to-action";
-// };
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :items
-  resources :bookings, only: %i[create update destroy]
+  resources :items do
+    resources :bookings, only: %i[show create update destroy]
+  end
   get 'account/:id/listings', to: 'pages#listings', as: :listings
   get 'account/:id/purchases', to: 'pages#purchases', as: :purchases
   get 'account/:id/settings', to: 'pages#settings', as: :settings
-  resources :items
-  resources :bookings, only: %i[show create update destroy]
   get 'about', to: 'pages#about', as: :about
   get 'team', to: 'pages#team', as: :team
 end
