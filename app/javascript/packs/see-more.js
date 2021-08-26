305176a93b889efe5b8dd6dc35249f367de55171
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-08-26 17:06:47 +0100

    Updated wording within 'see more' button to change with event listener

--- app/javascript/packs/see-more.js
+++ app/javascript/packs/see-more.js
@@ -1,11 +1,14 @@
 const listenForSeeMore = () => {
   const seeMore = document.querySelector('.show-content');
   seeMore.addEventListener("click", (e) => {
-    const adjustBoxHeight = document.getElementById('info1');
     e.preventDefault();
-    console.log(adjustBoxHeight);
+    let button = seeMore.querySelector('a');
+    if (button.innerText === "..Show more") {
+      button.innerText = "Show less";
+    } else {
+      button.innerText = "..Show more";
+    };
     document.getElementById('hide').classList.toggle('hide-content');
-
   });
 };
 
