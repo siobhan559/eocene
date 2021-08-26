d258009619c6ea82dd89bb180d0f79c6ba4c77ab
Cesar Ades <kito@ades.as>
2021-08-26 09:56:42 +0100

    Seed upgraded with four stock users and 10 set items

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,30 @@
 require "open-uri"
 LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 puts "---------------------------------------------"
-puts "Cleaning the database"
+puts "Cleaning database..."
+puts "---------------------------------------------"
 Item.delete_all
 User.delete_all
-puts "Database has been cleaned"
-puts "---------------------------------------------"
+
+#################################
+#####     Helper Method     #####
+#################################
+def attach_photo(object, url, item = true)
+  file = URI.open(url)
+  if item
+    object.photos.attach(io: file, filename: 'image.png', content_type: 'image/png')
+  else
+    object.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
+  end
+end
 
 ###################################
 #####     Creates 4 Users     #####
 ###################################
+puts "---------------------------------------------"
+puts "Creating users..."
+puts "---------------------------------------------"
+
 cesar = User.create(first_name: 'Cesar', last_name: 'Ades', password: 'password', email: 'cesar@seed.com')
 attach_photo(cesar, 'https://kitt.lewagon.com/placeholder/users/cesarades', false)
 
@@ -30,103 +45,88 @@ users = [cesar, george, daniella, siobhan]
 ####################################################
 #####     Creates 10 Items in 6 Categories     #####
 ####################################################
+puts "---------------------------------------------"
+puts "Creating items..."
+puts "---------------------------------------------"
 surfboard = Item.new(name: 'Surfboard',
                      category: 'Surf',
                      price: 23,
                      description: LOREM,
                      user: users.sample)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+attach_photo(surfboard, 'https://images.unsplash.com/photo-1475136863666-2149d427c1d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8c3VyZmJvYXJkfHx8fHx8MTYyOTk2Njg5OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(surfboard, 'https://images.unsplash.com/photo-1488462173947-c03971a64ec7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8c3VyZmJvYXJkfHx8fHx8MTYyOTk2NjkxNw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-skiboots = Item.new(
-  name: 'Ski Boots',
-  category: 'Winter Sports',
-  price: 10,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+skiboots = Item.new(name: 'Ski Boots',
+                    category: 'Winter Sports',
+                    price: 10,
+                    description: LOREM,
+                    user: users.sample)
+attach_photo(skiboots, 'https://www.powderlife.com/wp-content/uploads/2019/02/niseko-ski-boot-flex-boot-solutions-03.jpg')
+attach_photo(skiboots, 'https://images.unsplash.com/photo-1546180303-8cd6a7ef66b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8c2tpfHx8fHx8MTYyOTk2Njk1OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-wetsuit = Item.new(
-  name: 'Wet Suit',
-  category: 'Dive',
-  price: 8,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+wetsuit = Item.new(name: 'Wet Suit',
+                   category: 'Dive',
+                   price: 8,
+                   description: LOREM,
+                   user: users.sample)
+attach_photo(wetsuit, 'https://www.leisurepro.com/blog/wp-content/uploads/2018/06/shutterstock_274871849.jpg')
+attach_photo(wetsuit, 'https://images.unsplash.com/photo-1628791442883-3751c507a719?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2V0c3VpdHx8fHx8fDE2Mjk5NjcwNzI&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-snowboard = Item.new(
-  name: 'Snowboard',
-  category: 'Winter Sports',
-  price: 19,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+snowboard = Item.new(name: 'Snowboard',
+                     category: 'Winter Sports',
+                     price: 19,
+                     description: LOREM,
+                     user: users.sample)
+attach_photo(snowboard, 'https://images.unsplash.com/photo-1522445263200-1b4b91053db8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8c25vd2JvYXJkfHx8fHx8MTYyOTk2NzA5NA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(snowboard, 'https://images.unsplash.com/photo-1518608774889-b04d2abe7702?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8c25vd2JvYXJkfHx8fHx8MTYyOTk2NzE3NA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-parachute = Item.new(
-  name: 'Parachute',
-  category: 'Other',
-  price: 26,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+parachute = Item.new(name: 'Parachute',
+                     category: 'Other',
+                     price: 26,
+                     description: LOREM,
+                     user: users.sample)
+attach_photo(parachute, 'https://images.unsplash.com/photo-1620589229333-0bbc4e777a4f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8cGFyYWNodXRlfHx8fHx8MTYyOTk2NzE4OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(parachute, 'https://images.unsplash.com/photo-1562278996-23d19978be53?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8cGFyYWNodXRlfHx8fHx8MTYyOTk2NzIwMQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-backpack = Item.new(
-  name: 'Backpack',
-  category: 'Trek',
-  price: 5,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+backpack = Item.new(name: 'Backpack',
+                    category: 'Trek',
+                    price: 5,
+                    description: LOREM,
+                    user: users.sample)
+attach_photo(backpack, 'https://images.unsplash.com/photo-1559195690-236f5fab61a9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YmFja3BhY2t8fHx8fHwxNjI5OTY3MjI4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(backpack, 'https://images.unsplash.com/photo-1596116216974-4da48d14620f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YmFja3BhY2t8fHx8fHwxNjI5OTY3MjM4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-helmet = Item.new(
-  name: 'Helmet',
-  category: 'Bike',
-  price: 3,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+helmet = Item.new(name: 'Helmet',
+                  category: 'Bike',
+                  price: 3,
+                  description: LOREM,
+                  user: users.sample)
+attach_photo(helmet, 'https://images.unsplash.com/photo-1602614015538-89e50df1284c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aGVsbWV0fHx8fHx8MTYyOTk2NzI1NQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(helmet, 'https://images.unsplash.com/photo-1612311485995-72121b495abb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aGVsbWV0fHx8fHx8MTYyOTk2NzI3MA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-bikeframe = Item.new(
-  name: 'Bike Frame',
-  category: 'Bike',
-  price: 32,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+bikeframe = Item.new(name: 'Bike Frame',
+                     category: 'Bike',
+                     price: 32,
+                     description: LOREM,
+                     user: users.sample)
+attach_photo(bikeframe, 'https://images.unsplash.com/photo-1586481184655-a2bdb8e413ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YmlrZXx8fHx8fDE2Mjk5NjczMDA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(bikeframe, 'https://images.unsplash.com/photo-1555671264-19bbb3d58321?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YmlrZXx8fHx8fDE2Mjk5NjczNDc&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
-bikepedals = Item.new(
-  name: 'Bike Pedals',
-  category: 'Bike',
-  price: 11,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+bikepedals = Item.new(name: 'Bike Pedals',
+                      category: 'Bike',
+                      price: 11,
+                      description: LOREM,
+                      user: users.sample)
+attach_photo(bikepedals, 'https://bikepacking.com/wp-content/uploads/2013/11/2013-11-bike-touring-pedals-main.jpg')
+attach_photo(bikepedals, 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1/cliplesspedals-0-1515510722.jpg?crop=1.00xw:0.788xh;0,0.212xh&resize=1200:*')
 
-kayak = Item.new(
-  name: 'Kayak',
-  category: 'Other',
-  price: 11,
-  description: LOREM,
-  user: users.sample
-)
-attach_photo(surfboard, url)
-attach_photo(surfboard, url)
+kayak = Item.new(name: 'Kayak',
+                 category: 'Other',
+                 price: 11,
+                 description: LOREM,
+                 user: users.sample)
+attach_photo(kayak, 'https://images.unsplash.com/photo-1588472235309-a3f64708b7a6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8a2F5YWt8fHx8fHwxNjI5OTY3NTA4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
+attach_photo(kayak, 'https://images.unsplash.com/photo-1576723175466-420a9b5324b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8a2F5YWt8fHx8fHwxNjI5OTY3NTIx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')
 
 items = [surfboard, skiboots, wetsuit, snowboard, parachute, backpack, helmet, bikeframe, bikepedals, kayak]
 
@@ -138,31 +138,6 @@ items.each do |item|
   puts "Successfully added: #{item.user.first_name}'s #{item.name}"
 end
 
-25.times do
-  item = Item.new(
-    name: hash.sample[1],
-    description: 'This is a really good description for this item.',
-    price: (5..50).to_a.sample,
-    category: hash.sample[0],
-    user: [user1, user2].sample
-  )
-  2.times do
-    file = URI.open("https://source.unsplash.com/featured/?#{item.name}")
-    item.photos.attach(io: file, filename: 'image.png', content_type: 'image/png')
-  end
-  item.save!
-  puts "Successfully added item: #{item.name}"
-end
-
 puts "---------------------------------------------"
-puts "Added #{Item.count} items"
+puts "Seed complete"
 puts "---------------------------------------------"
-
-def attach_photo(object, url, item = true)
-  file = URI.open(url)
-  if item
-    object.photos.attach(io: file, filename: 'image.png', content_type: 'image/png')
-  else
-    object.photo.attach(io: file, filename: 'image.png', content_type: 'image/png')
-  end
-end
