f03f6e6b0fd6be263371e218ec5a608d560fc93e
Cesar Ades <kito@ades.as>
2021-08-27 12:10:06 +0100

    Added validations for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,22 @@ class Booking < ApplicationRecord
   belongs_to :user
   # The item holds the owner user
   belongs_to :item
+
+  validates :start_date, :end_date, presence: true
+  validates :positive_range
+  validates :available?
+
+  private
+
+  def positive_range
+    return if start_date < end_date
+  end
+
+  def available?
+    item_bookings = Booking.where(item_id: item.id)
+    item_bookings.each do |other|
+      return false unless other.end_date < start_date || other.start_date > end_date
+    end
+    return true
+  end
 end
