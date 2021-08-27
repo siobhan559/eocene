889724d95e2337f348212019634d4b7c7b255fde
Cesar Ades <kito@ades.as>
2021-08-27 15:16:09 +0100

    booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,8 @@ class BookingsController < ApplicationController
     @item = Item.find(params[:item_id])
     @booking.item = @item
     @booking.user = current_user
+    @booking.total_price = @item.price * (@booking.end_date - @booking.start_date).to_i
     if @booking.save
-      @booking.total_price = @item.price * (@booking.end_date - @booking.start_date).to_i
       redirect_to booking_path(@booking)
     else
       render "items/show"
@@ -16,6 +16,8 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
+    @days = (@booking.end_date - @booking.start_date).to_i
   end
 
   def destroy
